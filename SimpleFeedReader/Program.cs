25e0ae1f88abe070cde0a705ab547a94da9d4ccf
jacekm <jacekm@kainos.com>
2018-10-15 13:12:28 +0200

    Web App port change to 5050

--- SimpleFeedReader/Program.cs
+++ SimpleFeedReader/Program.cs
@@ -12,6 +12,7 @@ namespace SimpleFeedReader
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:5050");
     }
 }
